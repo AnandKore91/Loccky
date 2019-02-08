44fd2f68012b9df147308284f4e9b2a24a827269
Anand Kore <anand.kore@blancco.com>
2019-02-08 16:15:09 +0530

    - Validation issue resolved.

--- Loccky/Loccky-Manager/LocckyViewController.swift
+++ Loccky/Loccky-Manager/LocckyViewController.swift
@@ -12,6 +12,8 @@ public class LocckyViewController: UIViewController {
 
     //MARK:- Properties
     var appTitle:String = "My App"
+    fileprivate var arrPasscode = [String]()
+    fileprivate var arrTmpPwd = [String]()
     
     //MARK:- Outlets
     @IBOutlet weak var lblInstructions: UILabel!
@@ -31,6 +33,8 @@ public class LocckyViewController: UIViewController {
     @IBOutlet weak var btnNine: UIButton!
     @IBOutlet weak var btnZero: UIButton!
     
+    @IBOutlet weak var btnErase: UIButton!
+    
     @IBOutlet weak var imgFingerPrint: UIImageView!
     
     //MARK:- Class life cycle
@@ -40,6 +44,10 @@ public class LocckyViewController: UIViewController {
         //--- Setup ui
         configureButtonUI([btnOne, btnTwo, btnThree, btnFour, btnFive, btnSix, btnSeven, btnEight, btnNine, btnZero])
         
+        
+        for index in 0..<4{
+            print(index)
+        }
     }
 
     
@@ -47,65 +55,95 @@ public class LocckyViewController: UIViewController {
     
     @IBAction func passcodeEntered(_ sender: UIButton) {
         print("Btn Pressed: \(sender.tag)")
-        
-        switch sender {
-        case btnOne:
-            break
-        case btnTwo:
-            break
-        case btnThree:
-            break
-        case btnFour:
-            break
-        case btnFive:
-            break
-        case btnSix:
-            break
-        case btnSeven:
-            break
-        case btnEight:
-            break
-        case btnNine:
-            break
-        case btnZero:
-            break
-        default:
-            print("Invalid sender.")
-            break
+        if isPasswordSet(){
+            arrPasscode.append("\(sender.tag)")
+            if arrPasscode.count == 4{
+                if arrPasscode.joined() == getPasscode(){
+                    //--- Success.
+                    dismiss(animated: true, completion: nil)
+                }else{
+                    //--- Invalid Passcode.
+                    arrPasscode.removeAll()
+                }
+            }
+        }else{
+            arrPasscode.append("\(sender.tag)")
+            
+            if arrPasscode.count == 4{
+                if arrTmpPwd.isEmpty{
+                    arrTmpPwd.append(contentsOf: arrPasscode)
+                    arrPasscode.removeAll()
+                    lblInstructions.text = "Re-Enter Passcode"
+                }else if arrTmpPwd.joined() == arrPasscode.joined(){
+                    //--- Successfully added new passcode.
+                    setPasscode(arrPasscode.joined())
+                    dismiss(animated: true, completion: nil)
+                }else{
+                    arrPasscode.removeAll()
+                    arrTmpPwd.removeAll()
+                    lblInstructions.text = "Enter Passcode"
+                    //--- Invalid pwd. Reset.
+                }
+            }
         }
+        validatePasscodeCount()
         
+    }
+    
+    @IBAction func btnErasePressed(_ sender: UIButton) {
+        print("Btn erase pressed.")
+        arrPasscode.remove(at: arrPasscode.count - 1)
+        validatePasscodeCount()
     }
     
     @IBAction func btnFingerPrintPressed(_ sender: UITapGestureRecognizer) {
         print("Btn fingerprint Pressed")
     }
-    
+}
 
+//MARK:- Passcode validation
+extension LocckyViewController{
+    fileprivate func validatePasscodeCount() {
+        let arrTmpLbls:[UILabel] = [lblKeyOne, lblKeyTwo, lblKeyThree, lblKeyFour]
+        
+        //--- Reset
+        for lbl in arrTmpLbls{
+            lbl.text = "O"
+        }
+        
+        if arrPasscode.count <= arrTmpLbls.count{
+            for index in 0..<arrPasscode.count{
+                print(index)
+                let lbl = arrTmpLbls[index]
+                lbl.text = "X"
+            }
+        }
+    }
 }
 
 //MARK:- Passcode management.
 extension LocckyViewController{
     fileprivate static let passcodeKey = "my_passcode"
-    fileprivate func isPasswordSet() -> Bool{
+    @discardableResult fileprivate func isPasswordSet() -> Bool{
         if getPasscode() != nil{
             return true
         }
         return false
     }
     
-    fileprivate func setPasscode(_ passcode:String) -> Bool{
+    @discardableResult fileprivate func setPasscode(_ passcode:String) -> Bool{
         UserDefaults.standard.set(passcode, forKey: "my_passcode")
         return UserDefaults.standard.synchronize()
     }
     
-    fileprivate func getPasscode() -> String? {
+    @discardableResult fileprivate func getPasscode() -> String? {
         if let passcode = UserDefaults.standard.value(forKey: "my_passcode") as? String{
             return passcode
         }
         return nil
     }
     
-    fileprivate func deletePasscode() -> Bool{
+    @discardableResult fileprivate func deletePasscode() -> Bool{
         UserDefaults.standard.removeObject(forKey: "my_passcode")
         return UserDefaults.standard.synchronize()
     }
@@ -114,12 +152,10 @@ extension LocckyViewController{
 //MARK:- UI Management
 extension LocckyViewController{
     fileprivate func configureButtonUI(_ btns:[UIButton]){
-        
         for btn in btns{
             btn.layer.cornerRadius = btn.frame.size.height/2
             btn.layer.borderColor = UIColor.white.cgColor
             btn.layer.borderWidth = 1
         }
-        
     }
 }
