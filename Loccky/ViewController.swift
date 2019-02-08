63612d0589a917baa45b00e541990b856b9377a4
Anand Kore <anand.kore@blancco.com>
2019-02-08 12:01:20 +0530

    Initial Commit

---
+++ Loccky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Loccky
+//
+//  Created by Anand Kore on 08/02/19.
+//  Copyright © 2019 Blancco Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
